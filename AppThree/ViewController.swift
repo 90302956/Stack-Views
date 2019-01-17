de562d459544d43aba1ede6d0763a57d49042c9d
Michael Werdal <theactualduane@gmail.com>
2019-01-17 11:26:17 -0600

    Add files via upload

---
+++ AppThree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppThree
+//
+//  Created by 90302956 on 9/11/18.
+//  Copyright © 2018 Michael Werdal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
